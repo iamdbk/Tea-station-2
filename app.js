b78ca2a8d1739dadb4c98a8fb1c94035b73857b8
Daivik B Kesav <iamdaivik@gmail.com>
2021-01-21 22:46:34 +0530

    Made the Contact section , and the footer. Havent made the page responsive though.

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const ham = document.querySelector(".hamburger");
 const menu = document.querySelector(".nav-bar");
+const form = document.querySelector(".form");
+
 ham.addEventListener("click", slide);
 function slide() {
   menu.classList.toggle("scale");
 }
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+});
